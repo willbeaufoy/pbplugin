8a5799df8105c362bf54e6c3d48b9cc917b936dd
Will Beaufoy <will@willbeaufoy.net>
2014-11-01 12:08:53 +0000

    Further improvements to matching and UI
    Firefox plugin
    ==============
    
    * Add spinning icon when clicked
    * Add 'show popup on hover' function. Currently each .pbtitle has a
    matching div after it.
    * Add different options for regex matching

--- ffx/data/contentscript.js
+++ ffx/data/contentscript.js
@@ -1,4 +1,8 @@
 var html = document.body.innerHTML;
+var pbtitles = '';
+var ftcount = 0;
+var pattern = '';
+var repl = '';
 
 self.port.emit('sendHTML', html);
 
@@ -8,45 +12,193 @@ self.port.on('updateHTML', function(changes) {
 
     // Add style information for PB
     var style = document.createElement('style');
-    var css = '.pbtitle { background-color: yellow } .pbpopup { display: none; position: absolute; z-index: 999; max-width: 500px; background-color: #fff; padding: 5px; border: 1px solid #999; border-radius: 5px; font-size: 14px; color: #333; }';
+    var css = ".pbtitle { background-color: yellow } .pbpopup { display:none; position:absolute; top:-20px; z-index:999; max-width:500px; background-color:#eee; padding:10px; border:1px solid #999; border-radius:5px; font-size:14px; font-weight:normal; color:#333; white-space: normal; } .pbpopop a, .pbpopup a:visited, .pbpopup a:link { color:#1a0dab; }";
+
     style.appendChild(document.createTextNode(css));
     document.head.appendChild(style);
 
+    // Get all elements that contain title as whole word/s. (This includes both outer (e.g. <html>, <body>), and inner elements)
+    //var regex = new RegExp('[^A-Za-z0-9]' + title + '[^A-Za-z0-9]');
+
+    /* jQuery recursive function. Works but slow and sometimes fails */
+
+    $.fn.findtitles = function(title, link, excerpt, word_count) {
+      ftcount++;
+      console.log('findtitles called ' + String(ftcount))
+      console.log(title)
+      console.log(link)
+      console.log(excerpt)
+      console.log(word_count)
+      console.log("\n")
+        //var pattern = new RegExp('[^A-Za-z0-9]' + title + '[^A-Za-z0-9]', 'g'),
+        
+
+        this.each(function() {
+          console.log('this.each called.');
+          console.log(this);
+            $(this).contents().each(function() {
+                if(this.nodeType === 3 && pattern.test(this.nodeValue)) {
+                    console.log('Found text node containing title');
+                    console.log(this.nodeType);
+                    console.log(this);
+                    console.log('Replacing');
+                    console.log("\n")
+
+                    $(this).replaceWith(this.nodeValue.replace(pattern, repl));
+                }
+                else if(!$(this).hasClass('pb')) {
+                  console.log('This is a new non-text node so we need to recurse');
+                  console.log(this.nodeType);
+                  console.log("\n")
+                  // console.log('Found element');
+                  // console.log(this);
+                  // console.log('Continuing');
+                  $(this).findtitles(title, link, excerpt, word_count);
+                }
+            });
+        });
+        return this;
+    };
+
+    /* JS recursive function. Doesn't work */
+
+    function highlight(element,title,link,excerpt,word_count) {
+      console.log(element);
+      if (element.childNodes.length > 0)
+        console.log('length greater than 0');
+        for (var i = 0; i < element.childNodes.length; i++) 
+            highlight(element.childNodes[i]);
+
+      if (element.nodeType == Node.TEXT_NODE && pattern.test(element.nodeValue)) {
+        console.log('FOund, replacing');
+        element.innerHTML = element.nodeValue.replace(pattern, repl);
+      }
+    }
+
+    /* Tree walker */
+
+    function walkerHighlight(el){
+      var n, a=[], walk=document.createTreeWalker(el,NodeFilter.SHOW_TEXT,null,false);
+      while(n=walk.nextNode()) a.push(n);
+      return a;
+    }
+
+    /* Tree walker 2 */
+    // Prob that it returns js nodes etc.
+    /* COuld try checking for style.display = 'block' */
+    function nativeTreeWalker() {
+      console.log('nativeTreeWalker called');
+      var walker = document.createTreeWalker(
+          document.body, 
+          NodeFilter.SHOW_TEXT, 
+          null, 
+          false
+      );
+
+      var node;
+      var textNodes = [];
+
+      while(node = walker.nextNode()) {
+        console.log('nextnode');
+        console.log(node.nodeValue);
+          if(node.nodeValue && pattern.test(node.nodeValue)) {
+            console.log('node matches');
+            textNodes.push(node.nodeValue);
+            $(node).replaceWith(node.nodeValue.replace(pattern, repl));
+          }
+      }
+    }
+
     for(change in changes) {
-      console.log(change);
       var title = changes[change].title;
       var link = changes[change].link;
       var excerpt = changes[change].excerpt;
       var word_count = changes[change].word_count;
 
-      // Get all elements that contain title as whole word/s.
-      var regex = new RegExp('[^A-Za-z0-9]' + title + '[^A-Za-z0-9]', 'g');
-      //var regex = new RegExp('\s*' + title + '[\s\,\.]*', 'g');
-      //var regex = new RegExp('[^\S]' + title + '[\s\,\.]*', 'g');
-      var elems = $('body').filter(function() {
-        return regex.test($(this).text());
-      });
+      pattern = new RegExp('\\b' + title + '\\b', 'g');
+        //var pattern = /\b + title + \b/g,
+      repl = "<span class='pb pbtitle'>" + title + "</span>" + "<div class='pb pbpopup'><p><a href='" + link + "' target='_blank'>" + title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div>";
 
-      // Filter these by excluding all elements that contain another element with title (ie. parent elements like body)
-      var replaceElems = [];
-      $.each(elems, function(index, value) {
-        var unique = true;
-        $.each(elems, function(subindex, subvalue) {
-          if($.contains(value, subvalue)) {
-            unique = false;
-          }
-        });
-        if(unique == true) {
-          replaceElems.push(value);
-        }
-      });
+      console.log('make change')
+      console.log(title)
+      console.log(link)
+      console.log(excerpt)
+      console.log(word_count)
+      console.log("\n")
+
+      //$('body').findtitles(title, link, excerpt, word_count);
+      //highlight(document.body)
+      nativeTreeWalker()
+      console.log("findtitles finished. Moving to next change\n");
+      // var elems = $('*').filter(function() {
+      //   return regex.test($(this).text());
+      // });
+
+      // console.log('elems.length: ' + String(elems.length));
+      // console.log(elems);
+
+      // Filter these by excluding all elements that contain another element in elems (ie. parent elements like body)
+
+      // Problem with this is that some parent elements do include relevant child elements but should still be included
+
+      // So why not do all the replacements in the body?
+      // var replaceElems = [];
+      // $.each(elems, function(index, value) {
+      //   var unique = true;
+      //   $.each(elems, function(subindex, subvalue) {
+      //     if($.contains(value, subvalue)) {
+      //       unique = false;
+      //     }
+      //   });
+      //   if(unique == true) {
+      //     replaceElems.push(value);
+      //   }
+      // });
 
       // Update the text in these filtered elements
-      $.each(replaceElems, function(index, value) {
-        console.log(value);
-        var replacehtml = value.innerHTML.replace(regex, " <span class='pbtitle'> " + title + "</span><div class='pbpopup'><p><a href='" + link + "'>" +  title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div>");
-        value.innerHTML = replacehtml
-      });
+      // var i = 0;
+      // $.each(replaceElems, function(index, value) {
+      //   var replaceHTML = value.innerHTML;
+
+        // // Do all occurences where title is a full word surrounded by spaces
+        // var regex = new RegExp(' ' + title + ' ', 'g');
+        // replaceHTML = replaceHTML.replace(regex, " <span class='pbtitle'>" + title + "</span> <div class='pbpopup'><p><a href='" + link + "'>" +  title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div>");
+
+        // // Do all occurences where title is right in an element e.g. <em>title</em>
+        // var regex = new RegExp('>' + title + '<', 'g');
+        // replaceHTML = replaceHTML.replace(regex, "><span class='pbtitle'>" + title + "</span><div class='pbpopup'><p><a href='" + link + "'>" +  title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div><");
+
+        // // Do all occurences where title is a whole word followed by full stop or comma e.g. " title,"
+        // var regex = new RegExp(' ' + title + '([,\.])', 'g');
+        // replaceHTML = replaceHTML.replace(regex, "> <span class='pbtitle'>" + title + "</span>" + "$1" + "<div class='pbpopup'><p><a href='" + link + "'>" +  title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div><");
+
+        // Combine all three above
+        // var regex = new RegExp('([ >])' + title + '([ <,\.])', 'g');
+        // replaceHTML = replaceHTML.replace(regex, "$1<span id='pbtitle" + i + "' class='pbtitle'>" + title + "</span>" + "<div id='pbpopup" + i + "' class='pbpopup'><p><a href='" + link + "' target='_blank'>" + title + " Powerbase page</a></p><p>" + excerpt + "</p><p>" + word_count + " words</p></div>$2");
+
+        // value.innerHTML = replaceHTML;
+        // i = i + 1;
+      //});
     }
-  }
+    console.log("All changes done. Moving to adding event listeners for popups\n");
+
+    // Get all instances of pb titles that have been found in the page
+    pbtitles = document.querySelectorAll('.pbtitle');
+
+    // For each one, add an event handler to show its associated popup on mouseenter (and an event handler on the popup to hide it on mouseleave)
+    for(var i = 0; i < pbtitles.length; i++) {
+      //var id = pbtitles[i].id.replace('pbtitle', '');
+      console.log('Adding event listener for below:');
+      console.log(pbtitles[i]);
+      console.log("\n");
+      pbtitles[i].addEventListener('mouseenter', function() {
+        var popup = $(this).next()[0]
+        popup.addEventListener('mouseleave', function() {
+          popup.style.display = 'none';
+        })
+        popup.style.display = 'block';
+      });
+    };
+  };
+  self.port.emit('finishedUpdating');
 });
--- ffx/lib/main.js
+++ ffx/lib/main.js
@@ -1,38 +1,65 @@
 var buttons = require('sdk/ui/button/action');
+var toggleButtons = require('sdk/ui/button/toggle');
 var tabs = require("sdk/tabs");
 var self = require("sdk/self");
 var Request = require("sdk/request").Request;
 
-var button = buttons.ActionButton({
+var button = toggleButtons.ToggleButton({
   id: "check-powerbase",
   label: "Check Powerbase",
   icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
+    "16": "./icon-16.gif",
+    "32": "./icon-32.gif",
+    "64": "./icon-64.gif"
   },
   onClick: handleClick
 });
 
 function handleClick(state) {
+
+  // Start icon spinning
+  //button.icon['16'] = './icon-spinning-16.gif';
+  button.state('window', {
+    icon: {
+    "16": "./icon-spinning-16.gif",
+    "32": "./icon-32.gif",
+    "64": "./icon-64.gif"
+    },
+  });
+  console.log('button: ' + String(button.icon['16']) + ' clicked');
+
   worker = tabs.activeTab.attach({
     contentScriptFile: 
       [self.data.url('jquery-1.11.1.min.js'), self.data.url('contentscript.js')]
   });
+
   worker.port.on('sendHTML', function(html) {
     // var uri = tabs.activeTab.url
     var uri = html;
-    console.log(uri);
+    //console.log(uri);
     //var query = 'http://pb.afnewsagency.org';
     var query = 'http://localhost:14590';
-    console.log(query);
+    //console.log(query);
     var request = Request({
       url: query,
       content: html,
       onComplete: function(response) {
+        console.log(' Request has completed. json below');
         console.log(response.json);
         worker.port.emit('updateHTML', response.json);
       }
     }).post();
+  });
+
+  worker.port.on('finishedUpdating', function() {
+    // Reset button to static state
+    console.log('finishedUpdating')
+    button.state('window', {
+      icon: {
+      "16": "./icon-16.gif",
+      "32": "./icon-32.gif",
+      "64": "./icon-64.gif"
+      },
+    });
   });
 };
